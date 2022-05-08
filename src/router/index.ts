a112d7fbc8a9df7b42eeeefa8caab5c24c1b39dd
DreamingCodes <dev@dreaming.codes>
2022-05-08 20:29:45 +0200

    Fixed router using an env for BASE_URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import GridView from '../views/GridView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.VITE_BASE_URL || import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
